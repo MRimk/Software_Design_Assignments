b683346deeffd9ba60bf67efffcf7cbe3a037df5
MarciusRim <martynas.rimkevicius2001@gmail.com>
2022-03-02 15:03:06 +0100

    Changes by Bahrw, start of Main class development

--- src/main/java/softwaredesign/Main.java
+++ src/main/java/softwaredesign/Main.java
@@ -1,7 +1,14 @@
 package softwaredesign;
 
+import com.google.gson.Gson;
+
 public class Main {
     public static void main (String[] args){
         System.out.println("Welcome to Software Design");
+
+        Gson gson = new Gson();
+
+        User user = gson.fromJson("src/main/resources/user-data", User.class);
+
     }
 }
