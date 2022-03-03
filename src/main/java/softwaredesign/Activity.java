1dc74d81863e3423af2e20a582cb720f45f0a8e2
MarciusRim <martynas.rimkevicius2001@gmail.com>
2022-03-03 11:58:39 +0100

    Starting GPX parser part and user data parser

--- src/main/java/softwaredesign/Activity.java
+++ src/main/java/softwaredesign/Activity.java
@@ -1,18 +1,25 @@
 package softwaredesign;
-//import me.himanshusoni.gpxparser
+import org.alternativevision.gpx.GPXParser;
+import org.alternativevision.gpx.beans.GPX;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 
 public class Activity {
-//    private GPX gpx
+    private GPX gpx;
     private SportTypes sport;
     private final User user;
 
-    public Activity(String gpxPath, SportTypes sport, User user){
+    public Activity(String gpxPath, SportTypes sport, User user)
+            throws IOException, ParserConfigurationException, SAXException {
         this.user = user;
         this.sport = sport;
-        getGPX(gpxPath);
-    }
-
-    private static void getGPX(String path){
-        //this is the place to parse GPX file and build the object
+        GPXParser parser = new GPXParser();
+        FileInputStream inputStream = new FileInputStream(gpxPath);
+        gpx = parser.parseGPX(inputStream);
     }
 }
--- src/main/java/softwaredesign/Main.java
+++ src/main/java/softwaredesign/Main.java
@@ -4,11 +4,48 @@ import com.google.gson.Gson;
 
 public class Main {
     public static void main (String[] args){
-        System.out.println("Welcome to Software Design");
-
         Gson gson = new Gson();
 
         User user = gson.fromJson("src/main/resources/user-data", User.class);
 
+    }
+
+    public static User fillUserInformation(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Name ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter Age ");
+        Integer age = scanner.nextInt();
+
+        System.out.println("Enter Weight ");
+        Double weight = scanner.nextDouble();
+
+        System.out.println("Enter Height ");
+        Integer height = scanner.nextInt();
+
+        User user = new User(name, age, weight, height, Gender.MALE);
+        user.pretty_print();
+
+        return user;
+    }
+
+    public static void saveUserData(String compiledJSON) {
+        try {
+            File myObj = new File("src/main/resources/user_data");
+            if (!myObj.createNewFile()) {
+                System.out.println("File already exist");
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Could not create file 'user_data'");
+        }
+
+        try (FileWriter writer = new FileWriter("src/main/resources/user_data")) {
+            writer.write(compiledJSON);
+        }
+        catch (IOException e) {
+            System.out.println("Could not write to file 'user_data'");
+        }
     }
 }
