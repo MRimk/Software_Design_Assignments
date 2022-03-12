dfb3ba6d11b68ed0c3104519f3263ecd723c2fe5
MarciusRim <martynas.rimkevicius2001@gmail.com>
2022-03-12 12:58:32 +0100

    fix metrics of sports

--- src/main/java/org/softwaredesign/helpers/SportToMetricsHelper.java
+++ src/main/java/org/softwaredesign/helpers/SportToMetricsHelper.java
@@ -12,8 +12,8 @@ public final class SportToMetricsHelper {
         //this is empty since it is a static method class
     }
     private static final Map<Sport, Metric[]> metricsOfSport = Map.ofEntries(
-            entry(Sport.RUNNING, new Metric[]{new Distance(), new Time(), new Elevation(), new Pace(), new Speed(), new HeartRate(), new Cadence(), new CaloriesBurnt()}),
-            entry(Sport.CYCLING, new Metric[]{new Distance(), new Time(), new Speed(), new Pace(), new HeartRate(), new Power(), new CaloriesBurnt()}),
+            entry(Sport.RUNNING, new Metric[]{new Distance(), new Time(), new Elevation(), new Pace(), new HeartRate(), new Cadence(), new CaloriesBurnt()}),
+            entry(Sport.CYCLING, new Metric[]{new Distance(), new Time(), new Speed(), new HeartRate(), new Power(), new CaloriesBurnt()}),
             entry(Sport.SWIMMING, new Metric[]{new Distance(), new Time(), new Pace(), new StrokeLength(), new CaloriesBurnt()}));
     public static Metric[] getSportMetrics(Sport sport){
         return metricsOfSport.get(sport);
